179c02ec73e47397b1022f17233e62aa6526330b
Jose Augusto Orsili <joseorsili@hotmail.com>
2023-09-27 11:52:15 -0300

    Proyecto con nuevo PIC

--- Software/main.asm
+++ Software/main.asm
@@ -1,7 +1,7 @@
 ;====================================================================
 ; DEFINICIONES
 ;====================================================================
-#include p16f877a.inc                ; Incluir el archivo de definición de registros
+#include p16f688.inc ; Incluir el archivo de definición de registros
 
 ;====================================================================
 ; VARIABLES
@@ -14,89 +14,100 @@ CBLOCK 0x20
     ASCII_L
     temperatura ; Variable para almacenar la lectura del sensor LM35
 ENDC
+
 ;====================================================================
 ; VECTORES DE RESET E INTERRUPCIÓN
 ;====================================================================
-      ; Vector de reinicio
-RST   code  0x0 
-      goto  Start
+RST   code  0x0
+    goto Start
+
 ;====================================================================
 ; SEGMENTO DE CÓDIGO
 ;====================================================================
 Start
-  BSF STATUS, RP0
-  BCF TRISB, 0 ; RB0 como salida (ventilador)
-  BCF TRISB, 1 ; RB1 como salida (calefactor)
-  BCF STATUS, RP0
+    ; Configurar pines (RC0: motor, RC1: calefactor, RC3-RC5: switches)
+    BSF STATUS, RP0
+    BCF TRISC, 0 ; RC0 como salida (motor)
+    BCF TRISC, 1 ; RC1 como salida (calefactor)
+    BSF TRISC, 3 ; RC3 como entrada (switch)
+    BSF TRISC, 4 ; RC4 como entrada (switch)
+    BSF TRISC, 5 ; RC5 como entrada (switch)
+    BCF STATUS, RP0
 
-  ; Restaurar el estado inicial (apagar ambos dispositivos)
-  BCF PORTB, 0 ; Apagar el ventilador
-  BCF PORTB, 1 ; Apagar el calefactor
+    ; Restaurar el estado inicial (apagar ambos dispositivos)
+    BCF PORTC, 0 ; Apagar el motor
+    BCF PORTC, 1 ; Apagar el calefactor
+    
+    ; Configurar RA0 como entrada analógica para el sensor LM35
+    BSF ANSEL, 0 ; Configurar RA0/AN0 como entrada analógica
+    BCF TRISA, 0 ; RA0 como entrada digital
 
-  ; Configurar ADC
-  BSF ADCON1, PCFG0 ; Configurar RA0/AN0 como entrada analógica (0000 para AN0)
-  BCF ADCON1, PCFG1
-  BCF ADCON1, PCFG2
-  BCF ADCON1, PCFG3
-  BCF ADCON0, ADON  ; Desactivar el módulo A/D para configurarlo
+    ; Desactivar el módulo A/D para configurarlo
+    BCF ADCON0, ADON
 
 BUCLE
-  ; Encender el módulo A/D
-  BSF ADCON0, ADON
+    ; Encender el módulo A/D
+    BSF ADCON0, ADON
 
-  ; Llamar a la subrutina para leer el sensor LM35
-  CALL ReadLM35
-  
-   ; Comparar con 24 grados Celsius (usando el valor 31 para esta explicación)
-   MOVLW 0x0D ; Calibracion del sensor
-   SUBWF temperatura, W ; Restar temperatura - 25 y dejar el resultado en W
-   BTFSC STATUS, C ; Si el resultado es negativo, la temperatura es menor a 24 grados
-   GOTO TEMPERATURA_BAJA ; Si es negativo, saltar a la rutina de calefacción
-   GOTO TEMPERATURA_ALTA ; Si no es negativo, saltar a la rutina de ventilador
+    ; Llamar a la subrutina para leer el sensor LM35
+    CALL ReadLM35
+
+    ; Leer el estado de los switches en RC3, RC4 y RC5
+    BTFSC PORTC, 3 ; Verificar RC3
+    GOTO SWITCH_RC3_ON
+    BTFSC PORTC, 4 ; Verificar RC4
+    GOTO SWITCH_RC4_ON
+    BTFSC PORTC, 5 ; Verificar RC5
+    GOTO SWITCH_RC5_ON
+    ; Si ninguno de los switches está a 1, usar la temperatura estándar (24 grados)
+    MOVLW 0x0D ; Calibración del sensor
+    SUBWF temperatura, W ; Restar temperatura - 25 y dejar el resultado en W
+    
+    BTFSS STATUS, C ; Si el resultado es positivo (temperatura mayor a 24 grados), saltar
+    GOTO TEMPERATURA_ALTA ; Si es positivo, ir a la rutina de ventilador
+    GOTO TEMPERATURA_BAJA ; Si no es positivo, ir a la rutina de calefacción
+
+SWITCH_RC3_ON
+    ; Código para cuando RC3 está activado
+    ; Temperatura estándar (24 grados)
+    MOVLW 0x0D ; Calibración del sensor
+    GOTO BUCLE ; Continuar en el bucle principal
+
+SWITCH_RC4_ON
+    ; Código para cuando RC4 está activado
+    MOVLW 0x1B ; Cambio de temperatura en 27 grados
+    ADDWF temperatura, F ; Sumar 27 grados
+    GOTO BUCLE ; Continuar en el bucle principal
+
+SWITCH_RC5_ON
+    ; Código para cuando RC5 está activado
+    MOVLW 0x0E ; Cambio de temperatura en 14 grados
+    ADDWF temperatura, F ; Sumar 14 grados
+    GOTO BUCLE ; Continuar en el bucle principal
 
 TEMPERATURA_BAJA
-  ; Encender el calefactor (apagar el ventilador)
-  BSF PORTB, 1 ; Encender calefactor
-  BCF PORTB, 0 ; Apagar ventilador
-  GOTO BUCLE ; Continuar en el bucle principal
+    ; Encender el calefactor (apagar el motor)
+    BSF PORTC, 1 ; Encender calefactor
+    GOTO BUCLE ; Continuar en el bucle principal
 
 TEMPERATURA_ALTA
-  ; Encender el ventilador (apagar el calefactor)
-  BSF PORTB, 0 ; Encender ventilador
-  BCF PORTB, 1 ; Apagar calefactor
-  GOTO BUCLE ; Continuar en el bucle principal
-
-; Subrutina para leer el sensor LM35
+    ; Encender el motor (apagar el calefactor)
+    BSF PORTC, 0 ; Encender motor
+    GOTO BUCLE ; Continuar en el bucle principal
+    
 ReadLM35
-  ; Leer LM3 conectado a RA0
-  BSF ADCON0, GO ; Iniciar la conversión A/D
+    ; Leer LM35 conectado a RA0
+    BSF ADCON0, GO ; Iniciar la conversión A/D
 
 WAIT_FOR_ADC
-  ; Esperar hasta que la conversión A/D esté completa
-  BTFSC ADCON0, GO_DONE
-  GOTO WAIT_FOR_ADC
+    ; Esperar hasta que la conversión A/D esté completa
+    BTFSC ADCON0, GO_DONE
+    GOTO WAIT_FOR_ADC
 
-  ; En este punto, la conversión A/D está completa, podemos obtener la temperatura
-  MOVF ADRESH, W ; Mover el valor de ADRESH a W
-  MOVWF temperatura ; Almacenar el valor en la variable temperatura
-  RETURN
-
-DELAY
-  ; Retardo simple
-  MOVLW 0xFF
-  MOVWF COUNT1
-LOOP1
-  MOVLW 0xFF
-  MOVWF COUNT2
-LOOP2
-  DECFSZ COUNT2, F
-  GOTO LOOP2
-  DECFSZ COUNT1, F
-  GOTO LOOP1
-  RETURN
-
-COUNT1 EQU 0x30
-COUNT2 EQU 0x31
+    ; En este punto, la conversión A/D está completa, podemos obtener la temperatura
+    MOVF ADRESH, W ; Mover el valor de ADRESH a W
+    MOVWF temperatura ; Almacenar el valor en la variable temperatura
+    RETURN
 
 ;====================================================================
-      END
+END
